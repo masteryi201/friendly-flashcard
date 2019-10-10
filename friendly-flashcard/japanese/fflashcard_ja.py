60cdd77b863b9f8b46625faaab8e4cc2373c91bd
masteryi201 <mg3gold2014@gmail.com>
2019-10-10 10:09:53 -0400

    Update and rename main.py to fflashcard_ja.py

--- friendly-flashcard/japanese/fflashcard_ja.py
+++ friendly-flashcard/japanese/fflashcard_ja.py
@@ -3,29 +3,29 @@ import os
 import pandas as pd
 import math
 from PyPDF2 import PdfFileMerger
-script, csv_file = argv
+script, csv_file, path_image = argv
 data_file = csv_file
 results_file = './temp/results.csv'
 def main (data_file) :
 	list_file = []
 	cp_data = pd.read_csv(data_file, header=0)
 	number_row = cp_data.shape[0]
-	num_page = math.ceil(float(number_row) / 12.0)
-	missing = num_page * 12 - number_row
+	num_page = math.ceil(float(number_row) / 8.0)
+	missing = num_page * 8 - number_row
 	cp_data.to_csv(results_file, index=False, header=False)
 	data_frames = pd.read_csv(results_file, header=None)
-	print(data_frames)	
 	if missing != 0.0 :
 		for i in range (int(missing)):
-			add_row(data_frames, [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '', ''])
+			add_row(data_frames, [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '', ''])
+	print(data_frames)
 	for i in range(int(num_page)) :
 		list_file.append('file_' + str(i) + '.pdf')
-		mycmd = 'python module.py ' + results_file + ' file_' + str(i)
+		mycmd = 'python kanji_module.py ' + results_file + ' file_' + str(i) + " " + path_image
 		os.system(mycmd)
-		data_frames = data_frames.iloc[12:]
+		data_frames = data_frames.iloc[8:]
 		data_frames.to_csv(results_file, index=False, header=False)
 	pooled(list_file)
-	#remove()
+	remove()
 def add_row(df, row):
     df.loc[-1] = row
     df.index = df.index + 1  
@@ -37,7 +37,7 @@ def pooled(list_file):
 	merger =  PdfFileMerger()
 	for files in pdf_files:
 		merger.append(path+files)
-	merger.write('output.pdf')
+	merger.write('../output/output.pdf')
 	merger.close()
 def remove():
 	remove_code = 'rm ./temp/*'
