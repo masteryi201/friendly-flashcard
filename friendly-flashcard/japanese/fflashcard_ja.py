1b1accdad00a87e459172f6df9846626cfeb653f
masteryi201 <mg3gold2014@gmail.com>
2019-11-06 03:36:37 +0700

    Update fflashcard_ja.py

--- friendly-flashcard/japanese/fflashcard_ja.py
+++ friendly-flashcard/japanese/fflashcard_ja.py
@@ -3,44 +3,85 @@ import os
 import pandas as pd
 import math
 from PyPDF2 import PdfFileMerger
-script, csv_file, path_image = argv
+script, type_module, csv_file, path_image = argv
 data_file = csv_file
 results_file = './temp/results.csv'
-layder = 10
-def main (data_file) :
-	list_file = []
-	cp_data = pd.read_csv(data_file, header=0)
-	number_row = cp_data.shape[0]
-	num_page = math.ceil(float(number_row) / layder)
-	missing = num_page * layder - number_row
-	cp_data.to_csv(results_file, index=False, header=False)
-	data_frames = pd.read_csv(results_file, header=None)
-	if missing != 0.0 :
-		for i in range (int(missing)):
-			add_row(data_frames, [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '', ''])
-	print(data_frames)
-	for i in range(int(num_page)) :
-		list_file.append('file_' + str(i) + '.pdf')
-		mycmd = 'python kanji_module.py ' + results_file + ' file_' + str(i) + " " + path_image
-		os.system(mycmd)
-		data_frames = data_frames.iloc[layder:]
-		data_frames.to_csv(results_file, index=False, header=False)
-	pooled(list_file)
-	remove()
-def add_row(df, row):
-    df.loc[-1] = row
-    df.index = df.index + 1  
-    return df.sort_index()
+remove()
+if type_module == 'kanji':
+	layder = 10
+	def main (data_file) :
+		list_file = []
+		cp_data = pd.read_csv(data_file, header=0)
+		number_row = cp_data.shape[0]
+		num_page = math.ceil(float(number_row) / layder)
+		missing = num_page * layder - number_row
+		cp_data.to_csv(results_file, index=False, header=False)
+		data_frames = pd.read_csv(results_file, header=None)
+		if missing != 0.0 :
+			for i in range (int(missing)):
+				add_row(data_frames, [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '', ''])
+		print(data_frames)
+		for i in range(int(num_page)) :
+			list_file.append('file_' + str(i) + '.pdf')
+			mycmd = 'python kanji_module.py ' + results_file + ' file_' + str(i) + " " + path_image
+			os.system(mycmd)
+			data_frames = data_frames.iloc[layder:]
+			data_frames.to_csv(results_file, index=False, header=False)
+		pooled(list_file)
+		#remove()
+	def add_row(df, row):
+	    df.loc[-1] = row
+	    df.index = df.index + 1  
+	    return df.sort_index()
 
-def pooled(list_file):
-	path = './temp/'
-	pdf_files = list_file
-	merger =  PdfFileMerger()
-	for files in pdf_files:
-		merger.append(path+files)
-	merger.write('../output/output.pdf')
-	merger.close()
-def remove():
-	remove_code = 'rm ./temp/*'
-	os.system(remove_code)
-main (data_file)
+	def pooled(list_file):
+		path = './temp/'
+		pdf_files = list_file
+		merger =  PdfFileMerger()
+		for files in pdf_files:
+			merger.append(path+files)
+		merger.write('../output/output.pdf')
+		merger.close()
+	def remove():
+		remove_code = 'rm ./temp/*'
+		os.system(remove_code)
+	main (data_file)
+elif type_module == 'bunbo':
+	layder = 10
+	def main (data_file) :
+		list_file = []
+		cp_data = pd.read_csv(data_file, header=0)
+		number_row = cp_data.shape[0]
+		num_page = math.ceil(float(number_row) / layder)
+		missing = num_page * layder - number_row
+		cp_data.to_csv(results_file, index=False, header=False)
+		data_frames = pd.read_csv(results_file, header=None)
+		if missing != 0.0 :
+			for i in range (int(missing)):
+				add_row(data_frames, [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '])
+		print(data_frames)
+		for i in range(int(num_page)) :
+			list_file.append('file_' + str(i) + '.pdf')
+			mycmd = 'python bunbo_module.py ' + results_file + ' file_' + str(i) + " " + path_image
+			os.system(mycmd)
+			data_frames = data_frames.iloc[layder:]
+			data_frames.to_csv(results_file, index=False, header=False)
+		pooled(list_file)
+		#remove()
+	def add_row(df, row):
+	    df.loc[-1] = row
+	    df.index = df.index + 1  
+	    return df.sort_index()
+
+	def pooled(list_file):
+		path = './temp/'
+		pdf_files = list_file
+		merger =  PdfFileMerger()
+		for files in pdf_files:
+			merger.append(path+files)
+		merger.write('../output/output.pdf')
+		merger.close()
+	def remove():
+		remove_code = 'rm ./temp/*'
+		os.system(remove_code)
+	main (data_file)
